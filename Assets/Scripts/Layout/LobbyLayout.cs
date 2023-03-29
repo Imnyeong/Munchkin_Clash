08513d0479d563403218038150b60d2c44f1c7ca
Imnyeong <lmh4873@gmail.com>
2023-03-29 23:47:13 +0900

    [230329] - Lobby Layout ScrollRect 에 Room Unit Instantiate

--- Assets/Scripts/Layout/LobbyLayout.cs
+++ Assets/Scripts/Layout/LobbyLayout.cs
@@ -8,13 +8,13 @@ public class LobbyLayout : BaseLayout
 {
     [SerializeField] InputField inputRoomName;
     [SerializeField] Button createButton;
-    [SerializeField] Button joinButton;
+    [SerializeField] ScrollRect scrollRect;
+    [SerializeField] GameObject roomUnit;
     public override DataManager.LayoutType LayoutType => DataManager.LayoutType.Lobby;
     // Start is called before the first frame update
     void Start()
     {
         createButton.onClick.RemoveAllListeners();
-        joinButton.onClick.RemoveAllListeners();
 
         createButton.onClick.AddListener(delegate
         {
@@ -22,12 +22,12 @@ public class LobbyLayout : BaseLayout
                 return;
             PhotonNetwork.CreateRoom(inputRoomName.text);
         });
-        joinButton.onClick.AddListener(delegate
-        {
-            if (inputRoomName.text.Equals(string.Empty))
-                return;
-            PhotonNetwork.JoinRoom(inputRoomName.text);
-        });
+        //joinButton.onClick.AddListener(delegate
+        //{
+        //    if (inputRoomName.text.Equals(string.Empty))
+        //        return;
+        //    PhotonNetwork.JoinRoom(inputRoomName.text);
+        //});
     }
     public override void OnJoinedRoom()
     {
@@ -43,6 +43,15 @@ public class LobbyLayout : BaseLayout
         for (int i = 0; i < roomList.Count; ++i)
         {
             Debug.Log($"{i.ToString()} 번째 방 이름 : {roomList[i].Name}");
+        }
+        for (int i = scrollRect.content.childCount - 1; i >= 0; i--)
+        {
+            GameObject.Destroy(scrollRect.content.GetChild(i).gameObject);
+        }
+        for (int i = 0; i < roomList.Count; ++i)
+        {
+            GameObject roomObject = Instantiate(roomUnit.gameObject, scrollRect.content);
+            roomObject.name = roomList[i].Name;
         }
     }
 }
